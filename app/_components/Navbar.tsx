50c99f70b4b61c05f3fb5d486fad9f1eaaaeee54
Harshal Raikwar <harshalraikwar07@gmail.com>
2023-10-07 20:44:43 +0530

    💄 Update Navbar UI

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
                 </button>
                 {showNavbar ? (
                     <div>
-                        <div className=" flex overflow-x-hidden mx-4 md:mx-8 h-screen overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none md:hidden"
+                        <div className=" flex overflow-x-hidden mx-4 md:mx-8 h-screen overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none lg:hidden"
                         >
                             <div className="relative my-4 mx-auto w-screen">
                                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-900 outline-none focus:outline-none text-white">
@@ -43,11 +43,11 @@ export default function Navbar() {
 
                                     <div className="grid place-items-center text-xl py-2 gap-2 w-full mb-4">
 
-                                        <Link href="/" passHref>
+                                        <Link href="/" passHref onClick={() => setShowNavbar(false)}>
                                             <span className="lg:inline-flex px-3 mx-3 py-2 rounded items-center justify-center hover:bg-secondary cursor-pointer text-white">Home</span>
                                         </Link>
 
-                                        <Link href="/upload" passHref>
+                                        <Link href="/upload" passHref onClick={() => setShowNavbar(false)}>
                                             <span className="lg:inline-flex px-3 mx-3 py-2 rounded items-center justify-center hover:bg-secondary cursor-pointer text-white">Upload Video</span>
                                         </Link>
 
